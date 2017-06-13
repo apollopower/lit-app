05ab9c2265af9f558b369a07588aa15f74e5ec6b
Apollo <erthaljonas@gmail.com>
2017-06-13 18:39:06 -0400

    Created Ideas controller and appropriate actions

---
+++ app/controllers/ideas_controller.rb
@@ -0,0 +1,52 @@
+class IdeasController < ApplicationController
+  def index
+    @ideas = Idea.all
+  end
+
+  def show
+    @idea = Idea.find(params[:id])
+  end
+
+  def new
+    @idea = Idea.new
+  end
+
+  def edit
+    @idea = Idea.find(params[:id])
+  end
+
+  def create
+    @idea = Idea.new(idea_params)
+
+    if @idea.save
+      redirect_to @idea
+    else
+      render 'new'
+    end
+  end
+
+  def udpate
+    @idea = Idea.find(params[:id])
+
+    if @idea.update(idea_params)
+      redirect_to @idea
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @idea = Idea.find(params[:id])
+    @idea.destroy
+
+    redirect_to ideas_path
+
+  end
+
+  private
+
+  def idea_params
+    params.require(:idea).permit(:title,:description)
+  end
+
+end
