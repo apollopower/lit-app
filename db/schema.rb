82a47e41294f9907146e5cc780e462898cb71986
Apollo <erthaljonas@gmail.com>
2017-06-13 18:21:07 -0400

    Corrected PostgreSQL error

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20170613211719) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "conversations", force: :cascade do |t|
     t.boolean "verify"
     t.datetime "created_at", null: false
