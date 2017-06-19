3e1b01b0c40fe5da326b9a8b18afdf7ff890b4b7
Annette Medina <amedina72285@gmail.com>
2017-06-19 18:46:49 -0400

    user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
   resources :ideas
   root 'ideas#index'
 
+
+
   get 'auth/facebook', as: "login"
   get 'auth/facebook/callback', to: 'sessions#create'
   get 'logout'   => 'sessions#destroy', as: "logout"
+
+  get 'userprofile' => 'user#show'
+
+  post 'userprofile' => 'user#update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170618004120) do
+ActiveRecord::Schema.define(version: 20170619210834) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,8 @@ ActiveRecord::Schema.define(version: 20170618004120) do
     t.datetime "oauth_expires_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "bio"
+    t.string "skills"
   end
 
 end
